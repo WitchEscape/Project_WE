6b1f10f277a50fcca14275f6d6ab64bfa5eceb39
SaDongHuck <tkehdgur0001@gmail.com>
2025-01-08 09:37:25 +0900

    [FEAT] : 1Stage 비밀번호, 퍼즐, 쪽지 컨텐츠 구현 !!

---
+++ Assets/7.WokrSpaces/SaDongHuck/Script/Dial/DialNum.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialNum : MonoBehaviour
+{
+    //각각의 UI에 1 ~ 9를 표시하시 위해서 알파벳 저장할 변수 배열
+    string[] dialChars;
+    //위 배열를 컨트롤하기 위한 배열 인덱스 변수
+    int dialCharIndex;
+
+    public TextMeshProUGUI dialCharTxt;
+    public NumDial dial;
+
+    void Awake()
+    {
+        dialCharIndex = 0;
+        dialChars = new string[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        dialCharTxt.text = dialChars[dialCharIndex];
+    }
+
+    public void NumUp()
+    {
+        dialCharIndex++;
+        if (dialCharIndex == dialChars.Length)
+        {
+            dialCharIndex = 0;
+            dialCharTxt.text = dialChars[dialCharIndex];
+        }
+        else
+        {
+            dialCharTxt.text = dialChars[dialCharIndex];
+        }
+
+        dial.AnswerCheck();
+    }
+
+    public void NumDown()
+    {
+        dialCharIndex--;
+        if (dialCharIndex < 0)
+        {
+            dialCharIndex = dialChars.Length - 1;
+            dialCharTxt.text = dialChars[dialCharIndex];
+        }
+        else
+        {
+            dialCharTxt.text = dialChars[dialCharIndex];
+        }
+
+        dial.AnswerCheck();
+    }
+}
