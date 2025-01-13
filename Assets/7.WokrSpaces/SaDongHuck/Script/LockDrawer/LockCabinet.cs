9d11c6c3873898098f341403d598e0ff37e76cf9
SaDongHuck <tkehdgur0001@gmail.com>
2025-01-13 13:22:38 +0900

    2Stage 서랍문 잠김 설정

---
+++ Assets/7.WokrSpaces/SaDongHuck/Script/LockDrawer/LockCabinet.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockCabinet : MonoBehaviour
+{
+    private HingeJoint joint;
+    private bool isLokced = true;
+    private void Start()
+    {
+        joint = GetComponent<HingeJoint>();
+        LockDrawer();
+    }
+
+    public void LockDrawer()
+    {
+        isLokced = true;
+        JointLimits limits = joint.limits;
+        limits.min = 0;
+        limits.max = 0;
+        joint.limits = limits;
+        joint.useLimits = true;
+        print("문이 잠겼음");
+    }
+    public void UnLockDrawer()
+    {
+        isLokced = false;
+        JointLimits limits = joint.limits;
+        limits.min = 0;
+        limits.max = 123;
+        joint.limits = limits;
+        joint.useLimits = false;
+    }
+}
