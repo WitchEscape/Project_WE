bf6a7faeab5877c84a448567bf0542c4d5d7f8dc
JDY_NOTE\jdy00 <sjm25148@gmail.com>
2025-01-16 17:36:36 +0900

    임시 커밋

---
+++ Assets/7.WokrSpaces/7220RR/Scripts/ForceGrabSetting.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ForceGrabSetting : MonoBehaviour
+{
+    [SerializeField]
+    private XRBaseInteractor interactor;
+
+    private void Awake()
+    {
+        if (interactor == null)
+            interactor = GetComponent<XRBaseInteractor>();
+    }
+
+    private void OnEnable()
+    {
+        if (interactor != null)
+        {
+            interactor.hoverEntered.AddListener(InteractorForceGrabSet);
+            interactor.hoverExited.AddListener(InteractorForceGrabUnSet);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (interactor != null)
+        {
+            interactor.hoverEntered.RemoveListener(InteractorForceGrabSet);
+            interactor.hoverExited.RemoveListener(InteractorForceGrabUnSet);
+        }
+    }
+
+    private void InteractorForceGrabSet(HoverEnterEventArgs arg)
+    {
+        if (arg.interactorObject.transform.TryGetComponent<XRRayInteractor>(out XRRayInteractor ray))
+        {
+            ray.useForceGrab = true;
+        }
+    }
+
+    private void InteractorForceGrabUnSet(HoverExitEventArgs arg)
+    {
+        if (arg.interactorObject.transform.TryGetComponent<XRRayInteractor>(out XRRayInteractor ray))
+        {
+            ray.useForceGrab = false;
+        }
+    }
+}
