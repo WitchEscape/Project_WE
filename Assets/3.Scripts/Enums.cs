fc1f13edd95cd81b31cdbd208941a1ad7a830037
JDY_NOTE\jdy00 <sjm25148@gmail.com>
2025-01-16 13:29:52 +0900

    JointLimit set

--- Assets/3.Scripts/Enums.cs
+++ Assets/3.Scripts/Enums.cs
@@ -2,7 +2,7 @@ public enum Languages { KR, EN }
 
 public enum Chapters { Lobby, Class535, Dormitory, PostionClass, Library, TeachersRoom }
 
-public enum PuzzleColor { Red, Green, Blue, White, Black, Gray, Yellow, Orange, Violet, Skyblue, Pink }
+public enum PuzzleColor { White, Red, Green, Blue, Black, Gray, Yellow, Orange, Violet, Skyblue, Pink }
 
 
 #region Puzzles
