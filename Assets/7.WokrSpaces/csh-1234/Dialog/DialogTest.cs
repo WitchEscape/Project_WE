a39d48d3524ea8c05021098d390d48a378b69804
qkrrbtjr808 <qkrrbtjr808@naver.com>
2025-01-17 13:20:19 +0900

    Dialogduration 작업 1차 완료
    폰트가 깨지는 형상 수정 요망

--- Assets/7.WokrSpaces/csh-1234/Dialog/DialogTest.cs
+++ Assets/7.WokrSpaces/csh-1234/Dialog/DialogTest.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class DialogTest : MonoBehaviour
 {
     public TextMeshProUGUI dialogText;
-    
+
     private Coroutine CoTyping;
 
     [Header("체크시 한글자씩 나옵니다!")]
@@ -23,22 +23,52 @@ public class DialogTest : MonoBehaviour
     public string DialogId2;
     [Header("대사3")]
     public string DialogId3;
+    [Header("대사4")]
+    public string DialogId4;
+    [Header("대사5")]
+    public string DialogId5;
+    [Header("대사6")]
+    public string DialogId6;
+    [Header("대사7")]
+    public string DialogId7;
+    [Header("대사8")]
+    public string DialogId8;
+    [Header("대사9")]
+    public string DialogId9;
+    [Header("대사10")]
+    public string DialogId10;
+
 
     [Header("버튼입니다")]
     public Button Dialog1;
     public Button Dialog2;
     public Button Dialog3;
+    public Button Dialog4;
+    public Button Dialog5;
+    public Button Dialog6;
+    public Button Dialog7;
+    public Button Dialog8;
+    public Button Dialog9;
+    public Button Dialog10;
     void Start()
     {
         Dialog1.onClick.AddListener(StartDialog1);
         Dialog2.onClick.AddListener(StartDialog2);
         Dialog3.onClick.AddListener(StartDialog3);
+        Dialog4.onClick.AddListener(StartDialog4);
+        Dialog5.onClick.AddListener(StartDialog5);
+        Dialog6.onClick.AddListener(StartDialog6);
+        Dialog7.onClick.AddListener(StartDialog7);
+        Dialog8.onClick.AddListener(StartDialog8);
+        Dialog9.onClick.AddListener(StartDialog9);
+        Dialog10.onClick.AddListener(StartDialog10);
+
 
         DialogPlayer.Instance.OnDialogStart.AddListener((dialog) =>
         {
-            if(dialog.DialogType == DialogType.Story)
+            if (dialog.DialogType == DialogType.Story)
             {
-                if(isAnim == true)
+                if (isAnim == true)
                 {
                     if (CoTyping != null)
                     {
@@ -50,7 +80,7 @@ public class DialogTest : MonoBehaviour
                 {
                     dialogText.text = $"{dialog.SpeakerName} : {dialog.Text}";
                 }
-                
+
             }
             else
             {
@@ -84,11 +114,11 @@ public class DialogTest : MonoBehaviour
     {
         string fullText = $"{speaker} : {text}";
         dialogText.text = "";
-        
+
         string currentText = "";
         string currentTag = "";
         bool isTag = false;
-        
+
         foreach (char letter in fullText)
         {
             if (letter == '<')
@@ -97,7 +127,7 @@ public class DialogTest : MonoBehaviour
                 currentTag += letter;
                 continue;
             }
-            
+
             if (isTag)
             {
                 currentTag += letter;
@@ -112,7 +142,7 @@ public class DialogTest : MonoBehaviour
 
             currentText += letter;
             dialogText.text = currentText;
-            
+
             if (!isTag)
             {
                 yield return new WaitForSeconds(typingSpeed);
@@ -129,7 +159,7 @@ public class DialogTest : MonoBehaviour
         {
             StopCoroutine(CoTyping);
             CoTyping = null;
-            
+
             var currentDialog = DialogPlayer.Instance.GetCurrentDialog();
             if (currentDialog != null)
             {
@@ -152,4 +182,34 @@ public class DialogTest : MonoBehaviour
     {
         DialogPlayer.Instance.PlayDialogSequence(DialogId3);
     }
+    public void StartDialog4()
+    {
+        DialogPlayer.Instance.PlayDialogSequence(DialogId4);
+    }
+    public void StartDialog5()
+    {
+        DialogPlayer.Instance.PlayDialogSequence(DialogId5);
+    }
+    public void StartDialog6()
+    {
+        DialogPlayer.Instance.PlayDialogSequence(DialogId6);
+    }
+    public void StartDialog7()
+    {
+        DialogPlayer.Instance.PlayDialogSequence(DialogId7);
+    }
+    public void StartDialog8()
+    {
+        DialogPlayer.Instance.PlayDialogSequence(DialogId8);
+    }
+    public void StartDialog9()
+    {
+        DialogPlayer.Instance.PlayDialogSequence(DialogId9);
+    }
+    public void StartDialog10()
+    {
+        DialogPlayer.Instance.PlayDialogSequence(DialogId10);
+    }
+
+
 }
