9a32c78de7f5e376b4eb47900fc5f2bde17da332
JDY_NOTE\jdy00 <sjm25148@gmail.com>
2025-01-20 23:25:15 +0900

    2스테이지 로직 수정, 캐비닛 머테리얼 벼경

--- Assets/7.WokrSpaces/7220RR/Scripts/SocketLayerChange.cs
+++ Assets/7.WokrSpaces/7220RR/Scripts/SocketLayerChange.cs
@@ -5,9 +5,13 @@ public class SocketLayerChange : MonoBehaviour
 {
     [SerializeField]
     private XRSocketInteractor xrSocketInteractor;
-    private const int changeLayer = 32;
+    private const int changeLayer = 8;
+    private int baseInteractorLayer;
+    private int baseInteractableLayer;
+
     private void Awake()
     {
+        baseInteractorLayer = gameObject.layer;
         xrSocketInteractor ??= GetComponent<XRSocketInteractor>();
     }
 
@@ -15,7 +19,32 @@ public class SocketLayerChange : MonoBehaviour
     {
         if (xrSocketInteractor != null)
         {
-
+            xrSocketInteractor.selectEntered.AddListener(LayerChangeSet);
+            xrSocketInteractor.selectExited.AddListener(LayerChangeReset);
         }
+    }
+
+    private void OnDisable()
+    {
+        if (xrSocketInteractor != null)
+        {
+            xrSocketInteractor.selectEntered.RemoveListener(LayerChangeSet);
+            xrSocketInteractor.selectExited.RemoveListener(LayerChangeReset);
+        }
+    }
+
+    private void LayerChangeSet(SelectEnterEventArgs arg)
+    {
+        GameObject obj = arg.interactableObject.transform.gameObject;
+        baseInteractableLayer = obj.layer;
+        obj.layer = changeLayer;
+        gameObject.layer = changeLayer;
+    }
+
+    private void LayerChangeReset(SelectExitEventArgs arg)
+    {
+        GameObject obj = arg.interactableObject.transform.gameObject;
+        obj.layer = baseInteractableLayer;
+        gameObject.layer = baseInteractorLayer;
     }
 }
