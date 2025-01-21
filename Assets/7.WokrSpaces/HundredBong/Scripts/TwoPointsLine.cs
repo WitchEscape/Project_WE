7e82e663844e4e9a657b42919c0cf14491b42e32
HundredBong <kag2479@naver.com>
2025-01-22 02:30:32 +0900

    모든 씬 Player 및 Ghost 교체, 병합 충돌시 이 변경사항을 폐기할 것

--- Assets/7.WokrSpaces/HundredBong/Scripts/TwoPointsLine.cs
+++ Assets/7.WokrSpaces/HundredBong/Scripts/TwoPointsLine.cs
@@ -12,10 +12,25 @@ public class TwoPointsLine : MonoBehaviour
     [Header("라인 렌더러 시작점 (오브젝트)")] public Transform pointA;
     [Header("라인 렌더러 시작점 (툴팁)")] public Transform pointB;
     private LineRenderer line;
-
+    private CanvasGroup canvasGroup;
+    private Material lineMaterial;
     private void Awake()
     {
         line = GetComponent<LineRenderer>();
+    }
+
+    private void Start()
+    {
+
+            if (line != null)
+            {
+                // LineRenderer Material의 인스턴스 생성 (Instance)
+                lineMaterial = line.material;
+                lineMaterial = new Material(lineMaterial);
+                line.material = lineMaterial;
+            }
+        
+
     }
 
     private void Update()
@@ -23,6 +38,15 @@ public class TwoPointsLine : MonoBehaviour
         line.positionCount = 2;
         line.SetPosition(0, pointA.position);
         line.SetPosition(1, pointB.position);
+
+        #region 되면 컨펌받으러 가기
+        if (line != null && canvasGroup != null)
+        {
+            Color color = lineMaterial.color;
+            color.a = canvasGroup.alpha;
+            lineMaterial.color = color;
+        }
+        #endregion
     }
 
 }
