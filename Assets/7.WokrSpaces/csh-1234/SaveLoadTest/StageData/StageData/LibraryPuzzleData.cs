e1548435083f9782f9fe566a5dc3f40cee84f87a
csh-1234 <tmdtmdtmd4@naver.com>
2025-01-23 09:44:23 +0900

    5스테이지 임시 커밋(책상 조인트 수정 필요)

--- Assets/7.WokrSpaces/csh-1234/SaveLoadTest/StageData/StageData/LibraryPuzzleData.cs
+++ Assets/7.WokrSpaces/csh-1234/SaveLoadTest/StageData/StageData/LibraryPuzzleData.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class LibraryPuzzleData : StageDataBase
 {
     [SerializeField] private BookAnimationControll bookOpen;
-
+    
     private bool isActivated = false;
 
     protected override void Start()
--- Assets/7.WokrSpaces/csh-1234/SaveLoadTest/StageData/StageData/TeachersRoomPuzzleData.cs
+++ Assets/7.WokrSpaces/csh-1234/SaveLoadTest/StageData/StageData/TeachersRoomPuzzleData.cs
@@ -6,20 +6,31 @@ using UnityEngine;
 public class TeachersRoomPuzzleData : StageDataBase
 {
     [SerializeField] private Puzzles TeachersRoom_Puzzle_03;
+    [SerializeField] private BookAnimationControll bookOpen;
 
     protected override void Start()
     {
+        base.Start();
         TeachersRoom_Puzzle_03.ClearEvent.AddListener(TeachersRoom_Puzzle_03_Event);
+        bookOpen.OpenEvent.AddListener(LaffleBookOpen);
     }
 
+    
     private void TeachersRoom_Puzzle_03_Event()
     {
         if(PuzzleProgressManager.Instance.GetPuzzleState("TeachersRoom_Puzzle_03") == PuzzleProgressManager.PuzzleState.Completed)
         {
             DialogPlayer.Instance.PlayDialogSequence("TEACHERSROOM_03");
         }
-        
     }
+    private void LaffleBookOpen()
+    {
+        if (PuzzleProgressManager.Instance.GetPuzzleState("TeachersRoom_Puzzle_03") == PuzzleProgressManager.PuzzleState.Completed)
+        {
+            DialogPlayer.Instance.PlayDialogSequence("TEACHERSROOM_04");
+        }
+    }
+
 
     protected override void InitializePuzzleSequence()
     {
